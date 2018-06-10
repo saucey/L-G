5ea9e0578879d65f89f641efb8d8380aab59afae
leo scott <leoscott@leo-scotts-MacBook-Pro.local>
2018-06-10 21:53:46 +0100

    boilerplate working with jasmine test

---
+++ karma.conf.js
@@ -0,0 +1,52 @@
+module.exports = (config) => {
+  config.set({
+    // ... normal karma configuration
+    frameworks: ['jasmine'],
+
+    plugins: [
+      // 'karma-browserify',
+      'karma-jasmine',
+      'karma-webpack',
+      'karma-phantomjs-launcher',
+      'karma-chrome-launcher',
+    ],
+
+    files: [
+      'tests.webpack.js'
+      // all files ending in "_test"
+      // { pattern: '*.test.js', watched: false },
+      // { pattern: '**/*.test.js', watched: false }
+      // each file acts as entry point for the webpack configuration
+    ],
+
+    // exclude: [
+    //   'node_modules/**/*.js'
+    // ],
+
+    preprocessors: {
+      'tests.webpack.js': ['webpack']
+      // add webpack as preprocessor
+      // 'src/index.js': [ 'webpack' ],
+      // '**/*.test.js': [ 'webpack' ]
+    },
+
+    webpackMiddleware: {
+      noInfo: true
+    },
+    
+
+    webpack: {},
+    
+    autoWatch: true,
+    browsers: ['Chrome'],
+    // browsers: ['Chrome'],
+    webpackMiddleware: {
+      // webpack-dev-middleware configuration
+      // i. e.
+      stats: 'errors-only'
+    }
+
+
+
+  })
+}
---
+++ src/example.test.js
@@ -0,0 +1,10 @@
+
+import Reactssss from "react";
+
+describe('StopWatchComponent', () => {
+
+  it('should have its component name as default className', () => {
+    expect(1).toEqual(1);
+  });
+});
+
